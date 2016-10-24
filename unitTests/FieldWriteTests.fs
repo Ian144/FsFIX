4645cf12b1d8d564f05998f1a9b2ec7a4596c83b
Ian144 <ian144@hotmail.com>
2016-10-24 17:38:14 +0100

    added double field write test

--- unitTests/FieldWriteTests.fs
+++ unitTests/FieldWriteTests.fs
@@ -19,3 +19,19 @@ let ``write single-case DU test`` () =
     let arraysEq = expectedBytesWritten = dest2
     Assert.True (arraysEq, "unexpected bytes written by WriteBeginString")
     Assert.Equal (10, endPos)
+
+
+/// test that no part of the first is overwritten by the second
+[<Fact>]
+let ``write single-case DU test twice`` () =
+    let beginString:BeginString = BeginString.BeginString "FIX.4.4"
+    let dest = Array.zeroCreate<byte> 32
+    let endPos = WriteBeginString dest 0 beginString
+    let endPos2 = WriteBeginString dest endPos beginString
+
+    let expectedBytesWritten = [| yield! "8=FIX.4.4"B; yield 1uy; yield! "8=FIX.4.4"B; yield 1uy; |] // 1uy is the FIX delimiter
+    let dest2 = dest |> Array.take expectedBytesWritten.Length
+    let arraysEq = expectedBytesWritten = dest2
+    Assert.True (arraysEq, "unexpected bytes written by WriteBeginString")
+    Assert.Equal (20, endPos2)
+
