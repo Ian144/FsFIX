3dd3860b043cbf49097f5b91ad7e86842d9677f7
Ian144 <ian144@hotmail.com>
2016-10-29 14:32:02 +0100

    created, but not using generic WriteFieldXXX functions

--- fsFix/FieldFuncs.fs
+++ fsFix/FieldFuncs.fs
@@ -1,7 +1,7 @@
 ﻿module FieldFuncs
 
-
-
+open System
+open Conversions
 
 // todo: microbenchmark inlining this func
 let ReadSingleCaseDUIntField (pos:int) (bs:byte[]) fldCtor =
@@ -54,3 +54,84 @@ let ReadLengthDataCompoundField (strTagExpected:byte[]) (pos:int) (bs:byte[]) fl
     let strFieldBegin = strFieldTagValSepPos + 1
     let nextFieldTermPos2, bs = ByteArrayUtils.readNBytesVal strFieldBegin strLen bs
     nextFieldTermPos2 + 1, fldCtor bs
+
+
+
+let inline WriteFieldInt
+        (dest:byte []) 
+        (nextFreeIdx:int) 
+        (tag:byte[]) 
+        (fieldIn:^T) : int = 
+    let vv = (^T :(member Value:int) fieldIn)
+    Buffer.BlockCopy (tag, 0, dest, nextFreeIdx, tag.Length)
+    let nextFreeIdx2 = nextFreeIdx + tag.Length
+    let bs = Conversions.ToBytes.Convert(vv)
+    Buffer.BlockCopy (bs, 0, dest, nextFreeIdx2, bs.Length)
+    let nextFreeIdx3 = nextFreeIdx2 + bs.Length
+    dest.[nextFreeIdx3] <- 1uy // write the SOH field delimeter
+    nextFreeIdx3 + 1 // +1 to include the delimeter
+
+
+let inline WriteFieldDecimal
+        (dest:byte []) 
+        (nextFreeIdx:int) 
+        (tag:byte[]) 
+        (fieldIn:^T) : int = 
+    let vv = (^T :(member Value:decimal) fieldIn)
+    Buffer.BlockCopy (tag, 0, dest, nextFreeIdx, tag.Length)
+    let nextFreeIdx2 = nextFreeIdx + tag.Length
+    let bs = Conversions.ToBytes.Convert(vv)
+    Buffer.BlockCopy (bs, 0, dest, nextFreeIdx2, bs.Length)
+    let nextFreeIdx3 = nextFreeIdx2 + bs.Length
+    dest.[nextFreeIdx3] <- 1uy // write the SOH field delimeter
+    nextFreeIdx3 + 1 // +1 to include the delimeter
+
+
+let inline WriteFieldBool
+        (dest:byte []) 
+        (nextFreeIdx:int) 
+        (tag:byte[]) 
+        (fieldIn:^T) : int = 
+    let vv = (^T :(member Value:bool) fieldIn)
+    Buffer.BlockCopy (tag, 0, dest, nextFreeIdx, tag.Length)
+    let nextFreeIdx2 = nextFreeIdx + tag.Length
+    let bs = Conversions.ToBytes.Convert(vv)
+    Buffer.BlockCopy (bs, 0, dest, nextFreeIdx2, bs.Length)
+    let nextFreeIdx3 = nextFreeIdx2 + bs.Length
+    dest.[nextFreeIdx3] <- 1uy // write the SOH field delimeter
+    nextFreeIdx3 + 1 // +1 to include the delimeter
+
+// todo: how could one func replace the WriteFieldXXX funcs
+
+let inline WriteFieldStr 
+        (dest:byte []) 
+        (nextFreeIdx:int) 
+        (tag:byte[]) 
+        (fieldIn:^T) : int = 
+    let strVal = (^T :(member Value:string) fieldIn)
+    Buffer.BlockCopy (tag, 0, dest, nextFreeIdx, tag.Length)
+    let nextFreeIdx2 = nextFreeIdx + tag.Length
+    let bs = Conversions.ToBytes.Convert(strVal)
+    Buffer.BlockCopy (bs, 0, dest, nextFreeIdx2, bs.Length)
+    let nextFreeIdx3 = nextFreeIdx2 + bs.Length
+    dest.[nextFreeIdx3] <- 1uy // write the SOH field delimeter
+    nextFreeIdx3 + 1 // +1 to include the delimeter
+
+
+
+let inline WriteFieldData
+        (dest:byte []) 
+        (nextFreeIdx:int) 
+        (tag:byte[]) 
+        (fieldIn:^T) : int = 
+    let strVal = (^T :(member Value:string) fieldIn)
+    Buffer.BlockCopy (tag, 0, dest, nextFreeIdx, tag.Length)
+    let nextFreeIdx2 = nextFreeIdx + tag.Length
+    let bs = Conversions.ToBytes.Convert(strVal)
+    Buffer.BlockCopy (bs, 0, dest, nextFreeIdx2, bs.Length)
+    let nextFreeIdx3 = nextFreeIdx2 + bs.Length
+    dest.[nextFreeIdx3] <- 1uy // write the SOH field delimeter
+    nextFreeIdx3 + 1 // +1 to include the delimeter
+
+
+
