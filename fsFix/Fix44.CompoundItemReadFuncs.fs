f07795eba43bfeb2e05f38c0239bd6b7d7f694d1
Ian144 <ian144@hotmail.com>
2016-11-09 04:01:20 +0000

    read group experiment

--- fsFix/Fix44.CompoundItemReadFuncs.fs
+++ fsFix/Fix44.CompoundItemReadFuncs.fs
@@ -23,6 +23,17 @@ let inline ReadOptionalField (pos:int) (expectedTag:byte[]) (bs:byte[]) readFunc
         pos, None   // return the original pos, the next readoptional will re-read it
 
 
+
+let ReadGroup (ss:string) (pos:int) (numTag:byte[]) (bs:byte[]) readFunc = 
+    let pos2, tag = FIXBufUtils.readTag pos bs
+    if tag <> numTag then 
+        let msg = sprintf "when reading %s: expected tag: %A, actual: %A" ss numTag tag
+        failwith msg
+//    let pos3, fld = readFunc pos2 bs
+//    pos3, fld
+      pos2, []
+
+
 let inline ReadOptionalGroup (pos:int) (numFieldTag:byte[]) (bs:byte[]) readFunc = 
     let pos2, tag = FIXBufUtils.readTag pos bs
     if tag = numFieldTag then 
