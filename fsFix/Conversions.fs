1f7b71329c2b3d56d7be5907c58d0b8cd67a4c05
Ian144 <ian144@hotmail.com>
2016-12-18 10:30:49 +0000

    changed the wire representation of bools from 1 or 0 to Y or N as per FIX spec

--- fsFix/Conversions.fs
+++ fsFix/Conversions.fs
@@ -11,11 +11,10 @@ let bytesToUInt32 = bytesToStr >> System.Convert.ToUInt32
 
 
 let bytesToBool (bs:byte[]) =
-    let ii = bytesToInt32 bs
-    match ii with
-    | 0 ->  false
-    | 1 ->  true
-    | _ ->  failwith (sprintf "invalid value for bool field: %d" ii) 
+    match bs with
+    | "N"B ->  false
+    | "Y"B ->  true
+    | _ ->  failwith (sprintf "invalid value for bool field: %A" bs) 
     
 
 let bytesToDecimal (bs:byte[]) = 
@@ -29,13 +28,14 @@ let private sToB (ss:string) = System.Text.Encoding.UTF8.GetBytes ss
 
 
 
+
 [<AbstractClass;Sealed>]
 type ToBytes private () =
     static member Convert (str:string) = sToB str
     static member Convert (ii:int32)   = sprintf "%d" ii |> sToB     
     static member Convert (ui:uint32)  = sprintf "%d" ui |> sToB     
-    static member Convert (dd:decimal) = sprintf "%.32f" dd |> sToB  // todo: is "%.32f" ok for Decimal->string conversion, how does fix represent such types? what is thier byte representation
-    static member Convert (bb:bool)    = if bb then "1"B else "0"B   // todo: confirm this is how FIX sends bools down the wire
+    static member Convert (dd:decimal) = sprintf "%.32f" dd |> sToB  // see http://www.onixs.biz/fix-dictionary/4.4/
+    static member Convert (bb:bool)    = if bb then "Y"B else "N"B
     static member Convert (bs:byte []) = bs
     
 
