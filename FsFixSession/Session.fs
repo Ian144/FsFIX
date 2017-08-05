6e31dad14dced169195056f30b82d075245b2631
Ian144 <ian144@hotmail.com>
2017-08-05 13:18:36 +0100

    started session acceptor

---
+++ FsFixSession/Session.fs
@@ -0,0 +1,73 @@
+﻿module FsFix.Session
+
+
+open System.Net.Sockets
+open System.Threading;
+
+
+
+
+let HandleSocketError (name:string) (ex:System.Exception) = 
+    //let rec handleExInner (ex:System.Exception) = 
+    //    let msg = ex.Message
+    //    let optInnerEx = FSharpx.FSharpOption.ToFSharpOption ex.InnerException
+
+    //    match optInnerEx with
+    //    | None  ->          msg
+    //    | Some innerEx ->   let innerMsg = handleExInner innerEx
+    //                        sprintf "%s | %s" msg innerMsg
+    //let msg = handleExInner ex
+    //// Microsoft redis-benchmark does not close its socket connections down cleanly
+    //if not (msg.Contains("forcibly closed")) then
+    //    printfn "%s --> %s" name msg
+    ()
+
+
+let ClientError ex =  HandleSocketError "client error" ex
+let ConnectionListenerError ex = HandleSocketError "connection listener error" ex
+
+
+
+
+let AcceptorLoop (bufSize:int) (client:TcpClient) =
+    //todo: client.LingerState <-
+    client.NoDelay              <- true
+    client.ReceiveBufferSize    <- bufSize
+    client.SendBufferSize       <- bufSize
+    let strm = client.GetStream()
+    let buf = Array.zeroCreate<byte>(1024 * 64)
+    let threadFunc _ = 
+        while true do
+            let numBytes = strm.Read(buf, 0, bufSize )
+            
+            if numBytes = 0 then
+                () // timeout
+            else
+                () // process message
+
+
+            ()
+        ()
+    
+    let thread = Thread(ThreadStart(threadFunc))
+    thread.Start();
+    
+
+
+
+
+let ConnectionListenerLoop (bufSize:int) (listener:TcpListener) =
+    let asyncConnectionListener =
+        async {
+            while true do
+                let acceptClientTask = listener.AcceptTcpClientAsync ()
+                let! client = Async.AwaitTask acceptClientTask
+                do AcceptorLoop bufSize client
+        }
+
+    Async.StartWithContinuations(
+        asyncConnectionListener,
+        (fun _  -> printfn "ConnectionListener completed"),
+        ConnectionListenerError,
+        (fun ct -> printfn "ConnectionListener cancelled: %A" ct)
+    )
