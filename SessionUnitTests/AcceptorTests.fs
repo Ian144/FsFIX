0a2ac24ec8f02e15ad90ef88f43fdfe5ae45897b
Ian144 <ian144@hotmail.com>
2017-10-22 10:38:14 +0100

    added SessionUnitTests project

---
+++ SessionUnitTests/AcceptorTests.fs
@@ -0,0 +1,17 @@
+﻿module SessionUnitTests.Acceptor
+
+
+
+
+open Xunit
+open Swensen.Unquote
+
+
+
+
+
+
+
+[<Fact>]
+let ``acceptor process logon msg`` () =
+    true =! false
--- SessionUnitTests/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace SessionUnitTests
-
-type Class1() = 
-    member this.X = "F#"
